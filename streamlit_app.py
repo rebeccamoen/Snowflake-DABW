cc466433159353a5c061c8a1c1f595e5e8aedb53
Rebecca <54799857+rebeccamoen@users.noreply.github.com>
2022-11-16 12:47:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # Query the Account Metadata 
-'''my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+"""my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)'''
+streamlit.text(my_data_row)"""
 
 # Query Data from Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
