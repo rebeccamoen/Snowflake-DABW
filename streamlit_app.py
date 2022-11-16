16b6a833bb0dba233c875d7ce62f4119444de856
Rebecca <54799857+rebeccamoen@users.noreply.github.com>
2022-11-16 12:53:46 +0100

    Update streamlit_app.py
    Second Text Entry Box, allow the end user to add a fruit to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,9 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Second Text Entry Box, allow the end user to add a fruit to the list
+streamlit.header("Add fruit to the list:")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
